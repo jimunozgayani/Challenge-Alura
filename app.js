e98e007248d8663fc0abe7202ff4adf70f7fa4f9
Juan Ignacio Muñoz <ji.munozgayani@gmail.com>
2024-02-20 23:15:23 -0300

    crea la app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+const campo_texto = document.querySelector("#texto-encriptado");
+const campo_mensaje = document.querySelector("#campo-mensaje");
+
+const matriz_code = [
+    ["e", "enter"],
+    ["i", "imes"],
+    ["a", "ai"],
+    ["o", "ober"],
+    ["u", "ufat"],
+];
+
+
